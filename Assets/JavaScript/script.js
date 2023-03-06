73d502de42ca36e7b0797024ad09826fcf6abd5a
Shini <halimshini@hotmail.com>
2023-03-05 23:18:36 -0500

    Added array with question for quiz

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,12 +1,137 @@
-var button=document.querySelector('#button');
+// Dependencies
+var button=document.getElementById("button");
+var seconds=document.getElementById("seconds");
+var gameover=document.getElementById("gameover")
+var choices=document.getElementById("choices")
+var choice1=document.getElementById("choice1")
+var choice2=document.getElementById("choice2")
+var choice3=document.getElementById("choice3")
+var choice4=document.getElementById("choice4")
+var question=document.getElementById('question')
+var header=document.getElementById('header')
+var score=document.getElementById('score');
+var finalScore=document.getElementById('final-score')
+var initials=document.getElementById('initials')
+var submitBtn=document.querySelector('#submitBtn')
+var textArea=document.querySelector('#text-area')
+var score=document.querySelector('#score')
 
+var questionIndex=0;
+var checkAnswer=true;
+var secondsLeft=50;
 
+//Hide Elements 
+finalScore.classList.add('hiden');
+initials.classList.add('hiden');
+submitBtn.classList.add('hiden')
+textArea.classList.add('hiden')
+//ARRAY WITH QUESTIONS
+var questionArray=[ {
+    question:'Which of the following statements regarding JavaScript is true?',
+    choice1:'JavaScript is Assembly-language',
+    choice2:'JavaScript is an Object-Based language',
+    choice3:'JavaScript is a High-level language',
+    choice4:'JavaScript is an Object-Oriented language',
+    checkAnswer:choice2
+},
+{
+    question:'JavaScript was created by which company?',
+    choice1:'Ibm',
+    choice2:'Sun Microsystems',
+    choice3:'Netscape',
+    choice4:'Bell Labs',
+    checkAnswer:choice3
 
+},
+{
+    question:'Which of the below do not belong in the JavaScript Data Types category?',
+    choice1:'Number',
+    choice2:'Undefined',
+    choice3:'Float',
+    choice4:'Boolean',
+    checkAnswer: 'choice4 '
+},
+{
+    question:'Which of these String object functions gives the calling data type transformed to upper case?',
+    choice1:'toString()',
+    choice2:'substring()',
+    choice3:'toUpperCase()',
+    choice4:'toLocaleUpperCase()',
+    checkAnswer: 'choice3'
+}
+]
+//Start Game Function
 function startGame(){
   
-    
-   
+   button.classList.add("hiden");
+   header.classList.add('hiden');
+   game()
+   displayQuestions();
+}
+//Countdown Timer
+var interval;
+function game(){
+         interval=setInterval(function(){
+        secondsLeft--
+        seconds.textContent=secondsLeft +" "+ "Seconds Left";
+        if(secondsLeft===0){
+            message()
+            gameover.textContent="Game Over"; 
+            clearInterval(interval);
+           
+        }
+       },1000)
 }
 
+function displayQuestions(){
+    question.textContent=questionArray[questionIndex].question
+    choice1.textContent=questionArray[questionIndex].choice1
+    choice2.textContent=questionArray[questionIndex].choice2
+    choice3.textContent=questionArray[questionIndex].choice3
+    choice4.textContent=questionArray[questionIndex].choice4
+}
+//Function for checking correct answer
+function nextQuestion(event){
+    var buttontext=event.target.textContent;
+    console.log(buttontext)
+    if(buttontext!==questionArray[questionIndex].checkAnswer){
+        secondsLeft -=10;
 
-button.addEventListener('click',startGame());
+    }
+    questionIndex++
+   if(questionIndex<questionArray.length){
+        displayQuestions()
+    }
+    else{
+        message();        
+        score.textContent= " "+ secondsLeft;
+        clearInterval(interval)
+    }
+    
+}
+
+function message(){
+    question.classList.add('hiden');
+    choice1.classList.add('hiden');
+    choice2.classList.add('hiden');
+    choice3.classList.add('hiden');
+    choice4.classList.add('hiden');
+    finalScore.classList.remove('hiden');
+    initials.classList.remove('hiden');
+    submit.classList.remove('hiden');
+    textArea.classList.remove('hiden')
+}
+
+button.addEventListener('click',startGame);
+choice1.addEventListener('click',nextQuestion)
+choice2.addEventListener('click',nextQuestion)
+choice3.addEventListener('click',nextQuestion)
+choice4.addEventListener('click',nextQuestion)
+// submitBtn.addEventListener('submit',function(event){
+//     event.preventDefault;
+//     var msg=document.createElement('h3');
+//     var lastscore=document.createElement('p');
+//     msg.textContent="Hight Scores"
+//     lastscore.textContent=secondsLeft;
+//     document.body.appendChild(msg)
+//  })
